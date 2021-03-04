0e0bb230fe2d4490b24ea0b97e7e4db5e746538b
Scott Bauer <bauer.scott@mayo.edu>
2021-03-04 09:06:42 -0600

    EVSREPEXP-322. Redoing the tile based on feedback

--- clientui/src/components/SubHeaderBar.vue
+++ clientui/src/components/SubHeaderBar.vue
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-light navbar-expand-md navbar-dark justify-content-left evs-subheader">
        <a class="navbar-brand" > 
           <router-link v-bind:to="'/'" >
-             <img  alt="EVS Exporter" src="../assets/ExpTile.png"  width="65" height="38px" border="0">
+             <img  alt="EVS Exporter" src="../assets/ExpTile.jpg"  width="65" height="38px" border="0">
           </router-link>
       </a>
       <router-link v-bind:to="'/'" >Home</router-link>
